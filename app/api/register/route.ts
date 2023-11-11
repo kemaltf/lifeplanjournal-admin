32a16d043d9e64b4ae24bd9026235018c6afb48d
kemaltf <kemaltaufikf@gmail.com>
2023-11-12 01:59:11 +0700

    create api for register

---
+++ app/api/register/route.ts
@@ -0,0 +1,22 @@
+import bcrypt from 'bcrypt';
+import prisma from '@/lib/prismadb';
+import { NextResponse } from 'next/server';
+export async function POST(request: Request) {
+  const body = await request.json();
+  console.log(body);
+  const { username, first_name, last_name, email, password, phone_number } = body;
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      first_name,
+      last_name,
+      email,
+      hashed_password: hashedPassword,
+      phone_number,
+    },
+  });
+
+  return NextResponse.json(user);
+}
