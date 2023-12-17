3501b8736980e2dccd2d1efb5d30cdb1dc3cfa37
kemaltf <kemaltaufikf@gmail.com>
2023-12-17 14:01:20 +0700

    setup the sign-in page to redirect to home when login

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,16 @@
-import { LoginForm } from '@/components/ui/organism/loginForm/loginForm';
+import { redirect } from 'next/navigation';
 
-export default function LoginPage() {
+import { LoginForm } from '@/components/ui/organism/loginForm/loginForm';
+import getCurrentUser from '@/app/actions/getCurrentUser';
+
+export default async function LoginPage() {
+  const currentUser = await getCurrentUser();
+
+  // if not authorize redirect
+  if (currentUser?.username) {
+    redirect('/');
+  }
+
   return (
     <>
       <LoginForm />
